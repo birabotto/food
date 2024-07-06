db5d17dfabe454f4696adfdd9f84140b2e8a2dd0
Bira Botto <tibira.botto@gmail.com>
2024-07-06 17:26:18 -0400

    Fix import global.css

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Slot } from "expo-router";
-
+import "../styles/global.css";
 export default function RootLayout() {
   return <Slot />;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Test</Text>
+    <View className="w-full h-full flex justify-center items-center">
+      <Text className="text-xl text-red-500">Test bira</Text>
     </View>
   );
 }
