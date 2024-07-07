c60a4d7a6994a6bbd3b3f12ddc846571c97618ba
Bira Botto <tibira.botto@gmail.com>
2024-07-07 16:30:29 -0400

    Header is done

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,9 +4,19 @@ import React from "react";
 
 export default function Header() {
   return (
-    <View>
-      <Pressable>
+    <View className="w-full flex flex-row items-center justify-between">
+      <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
         <Ionicons name="menu" size={20} color="#121212" />
+      </Pressable>
+      <View className="flex flex-col items-center justify-center">
+        <Text className="text-center text-sm text-slate-800">Location</Text>
+        <View className="flex-row items-center justify-center gap-1">
+          <Feather name="map-pin" size={14} color="#FF0000" />
+          <Text className="text-lg font-bold">Toronto</Text>
+        </View>
+      </View>
+      <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+        <Feather name="bell" size={20} color="#121212" />
       </Pressable>
     </View>
   );
